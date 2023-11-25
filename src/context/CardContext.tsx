aee22f35ed3635f0505d0911ce77676493259f94
Mahmoud Almekdad <Mahmoudalmokadadtechnology@gmail.com>
2023-11-25 10:49:18 +0400

    modify product if already exist

--- src/context/CardContext.tsx
+++ src/context/CardContext.tsx
@@ -11,6 +11,11 @@ const cartReducer = (state: any, action: { type: string; payload?: any }): any =
         ...state,
         cart: [...state.cart, action.payload],
       };
+    case 'MODIFY_EXISTING_ITEM':
+      return {
+        ...state,
+        cart: action.payload,
+      };
     case 'REMOVE_FROM_CART':
       return {
         ...state,
@@ -41,7 +46,20 @@ const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, { cart: [] });
 
   const addToCart = (item: any) => {
-    dispatch({ type: 'ADD_TO_CART', payload: item });
+    const existingItemIndex = state.cart.findIndex((cartItem: any) => cartItem.id === item.id);
+
+    if (existingItemIndex !== -1) {
+      // If the product already exists in the cart, modify the existing item
+      const updatedCart = [...state.cart];
+      const existingItem = updatedCart[existingItemIndex];
+      existingItem.quantity += item.quantity;
+      existingItem.color = item.color; // Update with the new color
+      
+      dispatch({ type: 'MODIFY_EXISTING_ITEM', payload: updatedCart });
+    } else {
+      // If the product does not exist in the cart, add it
+      dispatch({ type: 'ADD_TO_CART', payload: item });
+    }
   };
 
   const removeFromCart = (productId: number) => {
